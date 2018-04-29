33f680d44d939974e2ac04bfd74b539ca55ac221
github username <github email address>
2018-04-29 18:21:40 -0400

    fixed CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,75 +1,54 @@
 class TasksController < ApplicationController
 
+#allows the view to access all the posts in the database through the instance variable @tasks
+#index action
   get '/tasks'do
-     @tasks = Task.all
-     erb :'tasks/index'
-   end
+    @tasks = Task.all
+    erb :'tasks/index'
   end
 
+#new action
+#request to load the form
   get '/tasks/new' do
-     if !logged_in?
-      redirect to '/login'
-     else
-       erb :'tasks/new'
-     end
-   end
-
-   get '/tasks/:id' do
-     @tasks = Task.find(params[:id])
-     if logged_in?
-       erb :"tasks/show"
-     else
-       redirect '/login'
-     end
-   end
-
-   get '/tasks/:id/edit' do
-      if !logged_in?
-        redirect to '/login'
-      else
-       if  @tasks = current_user.tasks.find_by(params[:id])
-        erb :"tasks/edit"
-      else
-        redirect to '/tasks'
-       end
-    end
-
-   post '/tasks' do
-     if logged_in? && params[:name].empty?
-       redirect '/tasks/create'
-     else
-       @tasks = Task.create(name: params[:name])
-       redirect "/tasks/#{@tasks.id}"
-      end
-    end
-
-
-  patch '/task/:id' do
-    if logged_in? && params[:name] == ""
-      redirect to "/task/#{params[:id]}/edit"
+    if logged_in?
+      erb :'tasks/create'
     else
-      @task = Task.find_by_id(params[:id])
-      if @task && @task.user == current_user
-        @task.update(name: params[:name])
-        redirect to "/task/#{@task.id}"
-      else
-        redirect to "/task/#{@task.id}/edit"
-      end
+      redirect to '/login'
     end
   end
-#
-#   delete '/task/:id/delete' do
-#     if logged_in?
-#       @task = Task.find_by_id(params[:id])
-#     if @task && @task.user == current_user
-#       @task.delete
-#     end
-#     redirect to '/task'
-#     else
-#     redirect to '/login'
-#   end
-# end
-# end
-#
+
+#create a new task
+  post '/tasks' do
+    @tasks = Task.create(:name => params[:name])
+    redirect "/tasks/#{@tasks.id}"
+  end
+
+  get '/tasks/:id' do
+    if logged_in?
+    @tasks = Task.find_by(params[:id])
+     erb :"tasks/show"
+   end
+  end
+
+#show action
+#load edit form
+  get '/tasks/:id/edit' do
+    @tasks = Task.find_by(params[:id])
+    erb :"tasks/edit"
+  end
+
+#edit action
+  patch '/tasks/:id' do
+     @tasks = Task.find_by(params[:id])
+     @tasks.name = params[:name]
+     @tasks.save
+     redirect to '/tasks'
+  end
+
+  delete '/tasks/:id/delete' do
+    @tasks = Task.find_by(params[:id])
+    @tasks.delete
+    redirect to '/tasks'
+  end
 
 end
