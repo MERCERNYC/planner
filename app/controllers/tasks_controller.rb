78c1a3cd3873d487b2db5e6cf84b1fffc902d3e8
github username <github email address>
2018-04-30 14:52:19 -0400

    fixed /signup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,10 @@ class TasksController < ApplicationController
 #allows the view to access all the posts in the database through the instance variable @tasks
 #index action
   get '/tasks'do #list all tasks
+    if logged_in?
     @tasks = current_user.tasks
     erb :'tasks/index'
+  end
   end
 
 #new action
@@ -20,7 +22,7 @@ class TasksController < ApplicationController
 
   get '/tasks/:id' do #show task
     if logged_in?
-    @tasks = Task.find_by(:id => params[:id])
+    @task = Task.find_by(:id => params[:id])
      erb :"tasks/show"
     else
       redirect '/login'
@@ -30,7 +32,8 @@ class TasksController < ApplicationController
 #create a new task
   post '/tasks' do
     if logged_in?
-      @tasks = Task.create(:name => params[:name])
+      @task = current_user.tasks.create(:name => params[:name])
+      flash[:notice] = "Your Task Has Been Succesfully Created"
       redirect "/tasks"
     else
       erb '/tasks/create'
@@ -41,8 +44,8 @@ class TasksController < ApplicationController
 #load edit form
   get '/tasks/:id/edit' do
     if logged_in?
-    @tasks = Task.find_by(:id => params[:id])
-    if current_user.id == @tasks.user_id
+    @task = Task.find_by(:id => params[:id])
+    if current_user.id == @task.user_id
     erb :"tasks/edit"
     else
      flash[:notice] = "You are not authorized to edit this task."
@@ -54,27 +57,28 @@ class TasksController < ApplicationController
 #edit action
   patch '/tasks/:id' do
     if logged_in?
-     @tasks = Task.find_by(:id => params[:id])
-     @tasks.name = params[:name]
-     if @tasks.save
+     @task = Task.find_by(:id => params[:id])
+     @task.name = params[:name]
+     if @task.save
      flash[:notice] = "Your Task Has Been Succesfully Updated"
      redirect to '/tasks'
     else
-     redirect to "/tasks/#{@tasks.id}/edit"
-   end
+     redirect to "/tasks/#{@task.id}/edit"
+    end
     end
   end
 
   delete '/tasks/:id/delete' do
     if logged_in?
-     @tasks = Task.find_by(:id => params[:id])
-    if current_user.id == @tasks.user_id
-      @tasks.delete
+     @task = Task.find_by(:id => params[:id])
+    if current_user.id == @task.user_id
+      @task.delete
       flash[:notice] = "Task successfully removed!"
       redirect to '/tasks'
     else
      redirect to '/login'
     end
+  end
   end
 
 end
