e07a7a0ef0224547fa00dd5fd38313534dbbea76
github username <github email address>
2018-05-04 13:11:13 -0400

    fixed indentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,19 +12,19 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
-    get '/' do
-      erb :index
+  get '/' do
+    erb :index
+  end
+
+  helpers do
+
+    def logged_in?
+      !!current_user
     end
 
-    helpers do
-
-      def logged_in?
-        !!current_user
-      end
-
-      def current_user
-        @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
-      end
-
+    def current_user
+      @current_user ||= User.find_by(:id => session[:user_id]) if session[:user_id]
     end
   end
+
+end
